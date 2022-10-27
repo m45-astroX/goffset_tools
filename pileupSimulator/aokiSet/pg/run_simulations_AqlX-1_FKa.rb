0f6d422f0e031aa9cd9eaff9a6579fda7adef87f
m45-astroX <yu.2876.2346@gmail.com>
2022-10-27 21:13:50 +0900

    all

--- pileupSimulator/aokiSet/pg/run_simulations_AqlX-1_FKa.rb
+++ pileupSimulator/aokiSet/pg/run_simulations_AqlX-1_FKa.rb
@@ -57,9 +57,9 @@ def run_simulation(num, random, output)
     sim.random_seed = random
     sim.verbose = 0
     sim.print_detector_info
-    sim.set_database(detector_configuration: "database/detector_configuration.xml",
-                   detector_parameters: "database/detector_parameters.xml")
-    sim.set_gdml "database/mass_model.gdml"
+    sim.set_database(detector_configuration: "./database/detector_configuration.xml",
+                   detector_parameters: "./database/detector_parameters.xml")
+    sim.set_gdml "./database/mass_model.gdml"
     sim.set_physics(hadron_hp: false, cut_value: 0.0001,
                   customized_em: true, electron_range_ratio: 0.01, electron_final_range: 1.0e-5)
     sim.set_primary_generator :AEObservationPrimaryGen, {
